314b097ff809d67cc288485755e015426bd887e4
Tyler Fisher <tfisher@politico.com>
2018-02-15 15:53:10 -0500

    autocomplete office in race admin

--- election/admin/__init__.py
+++ election/admin/__init__.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from election.models import (Candidate, CandidateElection, Election,
                              ElectionCycle, ElectionDay, ElectionType, Race)
+from .race import RaceAdmin
 from .election import ElectionAdmin
 from .candidate import CandidateAdmin
 
 
-admin.site.register(Race)
+admin.site.register(Race, RaceAdmin)
 admin.site.register(Election, ElectionAdmin)
 admin.site.register(CandidateElection)
 admin.site.register(ElectionDay)
