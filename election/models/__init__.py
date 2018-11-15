b09b120800dc5bb4ba7fe0edc8dc16c23a94b511
Tyler Fisher <tfisher@politico.com>
2018-11-15 12:05:42 -0500

    moving electionevent into election

--- election/models/__init__.py
+++ election/models/__init__.py
@@ -3,8 +3,9 @@ from .ballot_answer import BallotAnswer
 from .ballot_measure import BallotMeasure
 from .candidate_election import CandidateElection
 from .candidate import Candidate
+from .election import Election
 from .election_cycle import ElectionCycle
 from .election_day import ElectionDay
+from .election_event import ElectionEvent
 from .election_type import ElectionType
-from .election import Election
 from .race import Race
--- election/serializers/__init__.py
+++ election/serializers/__init__.py
@@ -4,6 +4,7 @@ from .ballot_measure import BallotMeasureSerializer
 from .candidate import CandidateSerializer
 from .election_cycle import ElectionCycleSerializer
 from .election_day import ElectionDaySerializer
+from .election_event import ElectionEventSerializer
 from .election_type import ElectionTypeSerializer
 from .election import ElectionSerializer
 from .race import RaceSerializer
