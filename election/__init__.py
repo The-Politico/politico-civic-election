da6a75d67744bd11a92df1169794748d9d4c19c6
Allan James Vestal <allanjamesvestal@gmail.com>
2019-08-23 12:05:58 -0400

    Updates to setup.py: Markdown README parsing, variable version number, binary version of psycopg2.

--- election/__init__.py
+++ election/__init__.py
@@ -1,1 +1,3 @@
-default_app_config = 'election.apps.ElectionConfig'
+__version__ = (0, 3, 3)
+
+default_app_config = "election.apps.ElectionConfig"
--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
-from setuptools import find_packages, setup
+# Imports from python.
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+
+# Imports from election.
+from election import __version__
+
+
+REPO_URL = "https://github.com/The-Politico/politico-civic-election/"
+
+PYPI_VERSION = ".".join(str(v) for v in __version__)
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
 
 setup(
     name="politico-civic-election",
-    version="0.3.3",
-    description="",
-    url="https://github.com/The-Politico/politico-civic-election",
+    version=PYPI_VERSION,
+    packages=find_packages(exclude=["docs", "tests", "example"]),
+    license="MIT",
+    description=(
+        "Manage elections, candidates and ballots, the POLITICO way."
+    ),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=REPO_URL,
+    download_url="{repo_url}archive/{version}.tar.gz".format(
+        **{"repo_url": REPO_URL, "version": PYPI_VERSION}
+    ),
     author="POLITICO interactive news",
     author_email="interactives@politico.com",
-    license="MIT",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -20,12 +44,11 @@ setup(
         "Topic :: Internet :: WWW/HTTP",
     ],
     keywords="",
-    packages=find_packages(exclude=["docs", "tests", "example"]),
     install_requires=[
         "django",
         "djangorestframework",
         "dj-database-url",
-        "psycopg2",
+        "psycopg2-binary",
         "politico-civic-entity",
         "politico-civic-geography",
         "politico-civic-government",
