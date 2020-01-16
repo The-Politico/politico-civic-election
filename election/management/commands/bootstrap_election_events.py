64ea4c034b89159d872df5984527fa0b5584e389
Allan James Vestal <allanjamesvestal@gmail.com>
2020-01-16 01:29:10 -0500

    Modernize 'bootstrap_election_events' management command: it now draws data from the 'us-elections' library, and creates model instances that duplicate data far less than before.

--- election/management/commands/bootstrap_election_events.py
+++ election/management/commands/bootstrap_election_events.py
@@ -9,7 +9,9 @@ from django.core.management.base import BaseCommand
 
 
 # Imports from other dependencies.
+from elections import ElectionYear
 from geography.models import Division
+from government.models import Party
 import pytz
 import requests
 from tqdm import tqdm
@@ -17,9 +19,12 @@ import us
 
 
 # Imports from election.
+from election.models.election_ballot import ElectionBallot
 from election.models import ElectionCycle
 from election.models import ElectionDay
 from election.models import ElectionEvent
+from election.models import ElectionType
+from election.utils.election_metadata import load_election_metadata
 
 
 class Command(BaseCommand):
@@ -27,21 +32,6 @@ class Command(BaseCommand):
         "Create election event instances based off "
         "current elections in the database"
     )
-
-    base_url = (
-        "https://raw.githubusercontent.com/The-Politico/"
-        "election-calendar/master/2018"
-    )
-    data = {
-        "primary_calendar": {"url": "p2018_federal.csv"},
-        "reference": {"url": "state_reference.csv"},
-        "primary_poll_closings": {"url": "p2018_federal_eday.csv"},
-        "primary_early_voting": {"url": "p2018_federal_ev.csv"},
-        "primary_registration": {"url": "p2018_federal_vr.csv"},
-        "general_poll_closings": {"url": "g2018_eday.csv"},
-        "general_early_voting": {"url": "g2018_ev.csv"},
-        "general_registration": {"url": "g2018_vr.csv"},
-    }
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -53,240 +43,164 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        print("Loading election events")
-        self.cycle, created = ElectionCycle.objects.get_or_create(
-            name=options["cycle"]
+        print(f"Loading elections for {options['cycle']}...")
+        print("")
+
+        election_year = ElectionYear(options["cycle"])
+        self.cycle, cycle_was_created = ElectionCycle.objects.get_or_create(
+            name=election_year.year
         )
 
-        for key, value in self.data.items():
-            r = requests.get("{0}/{1}".format(self.base_url, value["url"]))
-            reader = csv.DictReader(r.text.splitlines())
-            self.data[key]["csv"] = reader
+        self.get_needed_election_types()
 
-        for row in tqdm(list(self.data["reference"]["csv"])):
-            if row["is_state"] == "no":
-                continue
+        democratic_party = Party.objects.get(ap_code="Dem")
+        print("  - Creating Democratic primaries...")
+        for elex_data in tqdm(election_year.elections.primaries.democratic):
+            self.create_single_party_primary(elex_data, democratic_party)
+        print("    Done!")
+        print("")
 
-            state = row["state_code"]
+        republican_party = Party.objects.get(ap_code="GOP")
+        print("  - Creating Republican primaries...")
+        for elex_data in tqdm(election_year.elections.primaries.republican):
+            self.create_single_party_primary(elex_data, republican_party)
+        print("    Done!")
+        print("")
 
-            state_data = {}
-            state_data["code"] = state
-            state_data["name"] = us.states.lookup(row["state_code"]).name
+        all_party_primaries = [
+            primary_elex
+            for primary_elex in election_year.elections.primaries
+            if primary_elex.election_variant == "primary"
+            and primary_elex.election_openness == "all-party"
+        ]
+        print("  - Creating all-party primaries...")
+        for elex_data in tqdm(all_party_primaries):
+            self.create_all_party_primary(elex_data)
+        print("    Done!")
+        print("")
 
-            for key, value in self.data.items():
-                for data_row in value["csv"]:
-                    if state == data_row["state_code"]:
-                        state_data[key] = data_row
-                        break
+        print("  - Creating general elections...")
+        for elex_data in tqdm(election_year.elections.general_elections):
+            self.create_general_election(elex_data)
+        print("    Done!")
+        print("")
 
-            self.create_primaries(state_data)
-
-            if state != "LA":
-                self.create_general(state_data)
-
-        self.create_spare_elections()
-
-    def create_primaries(self, data):
-        division = Division.objects.get(code_components__postal=data["code"])
-
-        if data["primary_poll_closings"]["polls_close"]:
-            time_zones = us.states.lookup(data["code"]).time_zones
-            poll_closing_tz = "{0} {1}".format(
-                data["primary_calendar"]["p2018_federal_election_date"],
-                data["primary_poll_closings"]["polls_close"],
-            )
-            poll_closing_time = datetime.strptime(
-                poll_closing_tz, "%Y-%m-%d %I:%M:%S %p"
-            ).astimezone(pytz.timezone(time_zones[0]))
-        else:
-            poll_closing_time = None
-
-        if data["primary_calendar"]["p2018_federal_election_date"]:
-            election_day, created = ElectionDay.objects.get_or_create(
-                cycle=self.cycle,
-                date=data["primary_calendar"]["p2018_federal_election_date"],
-            )
-
-            ElectionEvent.objects.get_or_create(
-                election_day=election_day,
-                division=division,
-                label="{0} {1}".format(
-                    division.label, ElectionEvent.PRIMARIES
-                ),
-                event_type=ElectionEvent.PRIMARIES,
-                dem_primary_type=self.set_null_value(
-                    data["primary_calendar"].get(
-                        "p2018_federal_dem_election_type"
-                    )
-                ),
-                gop_primary_type=self.set_null_value(
-                    data["primary_calendar"].get(
-                        "p2018_federal_rep_election_type"
-                    )
-                ),
-                early_vote_start=self.set_null_value(
-                    data["primary_early_voting"].get(
-                        "p2018_federal_evip_start_date"
-                    )
-                ),
-                early_vote_close=self.set_null_value(
-                    data["primary_early_voting"].get(
-                        "p2018_federal_evip_close_date"
-                    )
-                ),
-                vote_by_mail_application_deadline=self.set_null_value(
-                    data["primary_early_voting"].get(
-                        "p2018_federal_vbm_application_deadline"
-                    )
-                ),
-                vote_by_mail_ballot_deadline=self.set_null_value(
-                    data["primary_early_voting"].get(
-                        "p2018_federal_ballot_return_date"
-                    )
-                ),
-                online_registration_deadline=self.set_null_value(
-                    data["primary_registration"].get(
-                        "p2018_federal_online_vr_deadline"
-                    )
-                ),
-                registration_deadline=self.set_null_value(
-                    data["primary_registration"].get(
-                        "p2018_federal_vr_deadline"
-                    )
-                ),
-                poll_closing_time=poll_closing_time,
-            )
-
-        if data["primary_calendar"]["p2018_runoff_federal_election_date"]:
-            election_day, created = ElectionDay.objects.get_or_create(
-                cycle=self.cycle,
-                date=data["primary_calendar"][
-                    "p2018_runoff_federal_election_date"
+    def get_needed_election_types(self):
+        self.election_types = {
+            "CAUCUS": ElectionType.objects.get_or_create(
+                label=dict(ElectionType.TYPES)[ElectionType.PARTISAN_CAUCUS],
+                short_label="Caucus",
+                slug=ElectionType.PARTISAN_CAUCUS,
+            )[0],
+            "FIREHOUSE_CAUCUS": ElectionType.objects.get_or_create(
+                label=dict(ElectionType.TYPES)[
+                    ElectionType.PARTISAN_FIREHOUSE_CAUCUS
                 ],
+                short_label="Firehouse Caucus",
+                slug=ElectionType.PARTISAN_FIREHOUSE_CAUCUS,
+            )[0],
+            "PRIMARY": ElectionType.objects.get_or_create(
+                label=dict(ElectionType.TYPES)[ElectionType.PARTISAN_PRIMARY],
+                short_label="Primary",
+                slug=ElectionType.PARTISAN_PRIMARY,
+            )[0],
+            "ALL_PARTY": ElectionType.objects.get_or_create(
+                label=dict(ElectionType.TYPES)[ElectionType.ALL_PARTY_PRIMARY],
+                short_label="All-party",
+                slug=ElectionType.ALL_PARTY_PRIMARY,
+            )[0],
+            "GENERAL": ElectionType.objects.get_or_create(
+                label=dict(ElectionType.TYPES)[ElectionType.GENERAL],
+                short_label="General",
+                slug=ElectionType.GENERAL,
+            )[0],
+        }
+
+    def create_single_party_primary(self, election_data, party):
+        try:
+            division = Division.objects.get(
+                code_components__postal=election_data.state.abbr
             )
+        except Division.DoesNotExist:
+            division = None
 
-            ElectionEvent.objects.get_or_create(
-                election_day=election_day,
-                division=division,
-                label="{0} {1}".format(
-                    division.label, ElectionEvent.PRIMARIES_RUNOFF
-                ),
-                event_type=ElectionEvent.PRIMARIES_RUNOFF,
-            )
-
-    def create_general(self, data):
-        division = Division.objects.get(code_components__postal=data["code"])
-
-        if data["general_poll_closings"]["polls_close"]:
-            time_zones = us.states.lookup(data["code"]).time_zones
-            poll_closing_tz = "2018-11-06 {0}".format(
-                data["general_poll_closings"]["polls_close"]
-            )
-            poll_closing_time = datetime.strptime(
-                poll_closing_tz, "%Y-%m-%d %I:%M:%S %p"
-            ).astimezone(pytz.timezone(time_zones[0]))
-        else:
-            poll_closing_time = None
-
-        election_day, created = ElectionDay.objects.get_or_create(
-            cycle=self.cycle, date=date(2018, 11, 6)
+        election_day, day_was_created = ElectionDay.objects.get_or_create(
+            cycle=self.cycle, date=election_data.election_date
         )
 
-        ElectionEvent.objects.get_or_create(
-            election_day=election_day,
+        raw_election_type = getattr(
+            election_data, "election_variant", election_data.election_type
+        )
+
+        if raw_election_type == "caucus":
+            election_type_choice = self.election_types["CAUCUS"]
+        elif raw_election_type == "firehouse-caucus":
+            election_type_choice = self.election_types["FIREHOUSE_CAUCUS"]
+        else:
+            election_type_choice = self.election_types["PRIMARY"]
+
+        election_event, evt_was_created = ElectionEvent.objects.get_or_create(
             division=division,
-            label="{0} {1}".format(division.label, ElectionEvent.GENERAL),
-            event_type=ElectionEvent.GENERAL,
-            early_vote_start=self.set_null_value(
-                data["general_early_voting"].get("g2018_evip_start_date")
-            ),
-            early_vote_close=self.set_null_value(
-                data["general_early_voting"].get("g2018_evip_close_date")
-            ),
-            vote_by_mail_application_deadline=self.set_null_value(
-                data["general_early_voting"].get(
-                    "g2018_vbm_application_deadline"
-                )
-            ),
-            vote_by_mail_ballot_deadline=self.set_null_value(
-                data["general_early_voting"].get("g2018_ballot_return_date")
-            ),
-            online_registration_deadline=self.set_null_value(
-                data["general_registration"].get("g2018_online_vr_deadline")
-            ),
-            registration_deadline=self.set_null_value(
-                data["general_registration"].get("g2018_vr_deadline")
-            ),
-            poll_closing_time=poll_closing_time,
+            election_day=election_day,
+            election_type=election_type_choice,
+            defaults=dict(notes=election_data.election_day_notes),
         )
 
-    def set_null_value(self, val):
-        if val == "":
-            return None
+        non_null_defaults = load_election_metadata(election_data)
+
+        election_blt, blt_was_created = ElectionBallot.objects.get_or_create(
+            election_event_id=election_event.id,
+            party_id=party.id,
+            defaults=non_null_defaults,
+        )
+
+    def create_all_party_primary(self, election_data):
+        division = Division.objects.get(
+            code_components__postal=election_data.state.abbr
+        )
+
+        election_day, day_was_created = ElectionDay.objects.get_or_create(
+            cycle=self.cycle, date=election_data.election_date
+        )
+
+        election_event, evt_was_created = ElectionEvent.objects.get_or_create(
+            division=division,
+            election_day=election_day,
+            election_type=self.election_types["ALL_PARTY"],
+            defaults=dict(notes=election_data.election_day_notes),
+        )
+
+        non_null_defaults = load_election_metadata(election_data)
+
+        election_blt, blt_was_created = ElectionBallot.objects.get_or_create(
+            election_event=election_event, defaults=non_null_defaults
+        )
+
+    def create_general_election(self, election_data):
+        division = Division.objects.get(
+            code_components__postal=election_data.state.abbr
+        )
+
+        election_day, day_was_created = ElectionDay.objects.get_or_create(
+            cycle=self.cycle, date=election_data.election_date
+        )
+
+        election_event, evt_was_created = ElectionEvent.objects.get_or_create(
+            division=division,
+            election_day=election_day,
+            election_type=self.election_types["GENERAL"],
+            defaults=dict(notes=election_data.election_day_notes),
+        )
+
+        non_null_defaults = load_election_metadata(election_data)
+
+        if election_data.state.abbr == "LA":
+            non_null_defaults[
+                "offices_elected"
+            ] = ElectionBallot.PRESIDENTIAL_OFFICE
         else:
-            return val
+            non_null_defaults["offices_elected"] = ElectionBallot.ALL_OFFICES
 
-    def create_spare_elections(self):
-        new_york = Division.objects.get(code_components__postal="NY")
-        ny_state_day, created = ElectionDay.objects.get_or_create(
-            cycle=self.cycle, date=date(2018, 9, 13)
-        )
-
-        ElectionEvent.objects.get_or_create(
-            election_day=ny_state_day,
-            division=new_york,
-            label="{0} {1} (state-level offices only)".format(
-                new_york.label, ElectionEvent.PRIMARIES
-            ),
-            event_type=ElectionEvent.PRIMARIES,
-            vote_by_mail_application_deadline=date(2018, 9, 6),
-            vote_by_mail_ballot_deadline=date(2018, 9, 12),
-            registration_deadline=date(2018, 8, 19),
-        )
-
-        georgia = Division.objects.get(code_components__postal="GA")
-
-        ga_runoff_day_fed, created = ElectionDay.objects.get_or_create(
-            cycle=self.cycle, date=date(2019, 1, 8)
-        )
-
-        ElectionEvent.objects.get_or_create(
-            election_day=ga_runoff_day_fed,
-            division=georgia,
-            label="{0} {1} (federal offices only)".format(
-                georgia.label, ElectionEvent.GENERAL_RUNOFF
-            ),
-            event_type=ElectionEvent.GENERAL_RUNOFF,
-        )
-
-        ga_runoff_day_state, created = ElectionDay.objects.get_or_create(
-            cycle=self.cycle, date=date(2018, 12, 4)
-        )
-
-        ElectionEvent.objects.get_or_create(
-            election_day=ga_runoff_day_state,
-            division=georgia,
-            label="{0} {1} (state offices only)".format(
-                georgia.label, ElectionEvent.GENERAL_RUNOFF
-            ),
-            event_type=ElectionEvent.GENERAL_RUNOFF,
-        )
-
-        louisiana = Division.objects.get(code_components__postal="LA")
-
-        la_general_day, created = ElectionDay.objects.get_or_create(
-            cycle=self.cycle, date=date(2018, 12, 8)
-        )
-
-        ElectionEvent.objects.get_or_create(
-            election_day=la_general_day,
-            division=louisiana,
-            label="{0} {1}".format(louisiana.label, ElectionEvent.GENERAL),
-            event_type=ElectionEvent.GENERAL,
-            early_vote_start=date(2018, 11, 24),
-            early_vote_close=date(2018, 12, 1),
-            vote_by_mail_application_deadline=date(2018, 12, 4),
-            vote_by_mail_ballot_deadline=date(2018, 12, 7),
-            online_registration_deadline=date(2018, 11, 17),
-            registration_deadline=date(2018, 11, 7),
+        election_blt, blt_was_created = ElectionBallot.objects.get_or_create(
+            election_event=election_event, defaults=non_null_defaults
         )
